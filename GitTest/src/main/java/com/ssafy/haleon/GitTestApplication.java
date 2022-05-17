ee81299fb0cdd71a917325fbefafe3fc4355af59
Hooon97 <97091891+Hooon97@users.noreply.github.com>
2022-05-17 13:32:55 +0900

    kdh : git test ver2.

--- GitTest/src/main/java/com/ssafy/haleon/GitTestApplication.java
+++ GitTest/src/main/java/com/ssafy/haleon/GitTestApplication.java
@@ -8,6 +8,7 @@ public class GitTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestApplication.class, args);
+		System.out.println("Hello GitHub!");
 	}
 
 }
